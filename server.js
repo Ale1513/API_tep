14177c9044e318a4d4677eebd4024388d94c0c77
EdoardoZambernardi <132917328+EdoardoZambernardi@users.noreply.github.com>
2023-05-08 21:20:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-const filePath = path.join(process.cwd(), 'api_cultura', 'cultura.json');
-const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+const filePathCultura = path.join(process.cwd(), 'api_cultura', 'cultura.json');
+const filePathLavoro = path.join(process.cwd(), 'api_cultura', 'lavoro.json');
+const jsonDataCultura = JSON.parse(fs.readFileSync(filePathCultura, 'utf8'));
+const jsonDataLavoro = JSON.parse(fs.readFileSync(filePathLavoro, 'utf8'));
 
 app.get('/api/cultura', (req, res) => {
-  let filteredData = jsonData;
+  let filteredData = jsonDataCultura;
 
   if (req.query.id) {
     filteredData = filteredData.filter(cultura => cultura.id == req.query.id);
@@ -19,6 +21,34 @@ app.get('/api/cultura', (req, res) => {
 
   if (req.query.tipo) {
     filteredData = filteredData.filter(cultura => cultura.tipo.toLowerCase().includes(req.query.tipo.toLowerCase()));
+  }
+
+  res.json(filteredData);
+});
+
+app.get('/api/lavoro', (req, res) => {
+  let filteredData = jsonDataLavoro;
+
+  if (req.query.regione) {
+    filteredData = filteredData.filter(lavoro => lavoro.regione.toLowerCase().includes(req.query.regione.toLowerCase()));
+  }
+  
+  if (req.query.tasso_occ_min && req.query.tasso_occ_max) {
+    filteredData = filteredData.filter(lavoro => {
+      return lavoro.tasso_occ >= parseInt(req.query.tasso_occ_min) && lavoro.tasso_occ <= parseInt(req.query.tasso_occ_max);
+    });
+  }
+  
+  if (req.query.tasso_dis_min && req.query.tasso_dis_max) {
+    filteredData = filteredData.filter(lavoro => {
+      return lavoro.tasso_dis >= parseInt(req.query.tasso_dis_min) && lavoro.tasso_dis <= parseInt(req.query.tasso_dis_max);
+    });
+  }
+
+  if (req.query.stipendio_min && req.query.stipendio_max) {
+    filteredData = filteredData.filter(lavoro => {
+      return lavoro.stipendi_lordi_media >= parseInt(req.query.stipendio_min) && lavoro.stipendi_lordi_media <= parseInt(req.query.stipendio_max);
+    });
   }
 
   res.json(filteredData);
